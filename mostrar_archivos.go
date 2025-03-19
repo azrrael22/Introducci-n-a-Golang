5af26000faeabafe6d1add6ed931746589a5393f
Adriaann <adriaannm.rodriguezr@uqvirtual.edu.co>
2025-03-19 16:58:30 -0500

    Filtra la lista de archivos para mostrar solo imágenes (.jpg, .jpeg, .png) en el directorio especificado.

--- mostrar_archivos.go
+++ mostrar_archivos.go
@@ -6,18 +6,17 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings" // Importación para manejo de cadenas
 )
 
 func main() {
 	// Configurar flag para el directorio
 	var directorio string
 
-	//d para especificar el directorio
-	//. para usar el directorio actual
+	// d para especificar el directorio, . para usar el directorio actual
 	flag.StringVar(&directorio, "d", ".", "Directorio a listar")
 
-	//Procesa los argumentos y asigna los valores a las variables correspondientes, en este caso
-	//a la variable directorio
+	// Procesa los argumentos y asigna los valores a las variables correspondientes, en este caso a la variable directorio
 	flag.Parse()
 
 	// Leer contenido del directorio
@@ -34,7 +33,7 @@ func main() {
 
 	fmt.Printf("Archivos en %s:\n", absPath)
 
-	// Listar solo los archivos (excluyendo directorios)
+	// Listar solo los archivos de imagen (.jpg, .jpeg, .png)
 	for _, file := range files {
 		info, err := file.Info()
 		if err != nil {
@@ -42,9 +41,12 @@ func main() {
 			continue
 		}
 
-		// Mostrar el nombre solo si es un archivo
+		// Mostrar el nombre solo si es un archivo y tiene una extensión de imagen
 		if !info.IsDir() {
-			fmt.Println("- " + file.Name())
+			lowerName := strings.ToLower(file.Name())
+			if strings.HasSuffix(lowerName, ".jpg") || strings.HasSuffix(lowerName, ".jpeg") || strings.HasSuffix(lowerName, ".png") {
+				fmt.Println("- " + file.Name())
+			}
 		}
 	}
 }
